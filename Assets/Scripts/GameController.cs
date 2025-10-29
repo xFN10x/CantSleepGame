96b736644bb1b447e6ad018c27d823fce0e0c0e1
_FN10_ <mathdudex09@gmail.com>
2025-10-29 20:45:00 -0300

    Add door knob, and start begining cutscene

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,30 @@
+using DG.Tweening;
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public Camera CutsceneCamera;
+    public Image FadePanel;
+    public TextMeshProUGUI DialougeText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(StartingCutscene());
+    }
+
+    IEnumerator StartingCutscene()
+    {
+        yield return new WaitForSeconds(3);
+        yield return FadePanel.DOFade(1f, 3f).WaitForCompletion();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
